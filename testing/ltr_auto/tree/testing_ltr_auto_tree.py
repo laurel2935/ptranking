ec0a16604ce1ea8064e63160b7ef15cf6f33e945
ryo <s1511486@u.tsukuba.ac.jp>
2021-02-12 09:35:07 +0900

    update ltr_auto

--- testing/ltr_auto/tree/testing_ltr_auto_tree.py
+++ testing/ltr_auto/tree/testing_ltr_auto_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 """Description
 A simple script for testing either in-built methods or newly added methods with automatic parameter-tuning
 """
@@ -13,25 +14,21 @@ import numpy as np
 import torch
 
 from ptranking.ltr_global import ltr_seed
-from ptranking.ltr_auto.adhoc.ltr_auto_adhoc import AutoLTREvaluator
+from ptranking.ltr_auto.tree.ltr_auto_tree import AutoTreeLTREvaluator
 
 np.random.seed(seed=ltr_seed)
-torch.manual_seed(seed=ltr_seed)
+
 
 if __name__ == '__main__':
+
     """
-    >>> Learning-to-Rank Models <<<
-    (1) Optimization based on Empirical Risk Minimization
+    >>> Tree-based Learning-to-Rank Models <<<
+    
+    (3) Tree-based Model
     -----------------------------------------------------------------------------------------
-    | Pointwise | RankMSE                                                                   |
+    | LightGBMLambdaMART                                                                    |
     -----------------------------------------------------------------------------------------
-    | Pairwise  | RankNet                                                                   |
-    -----------------------------------------------------------------------------------------
-    | Listwise  | LambdaRank % ListNet % ListMLE % RankCosine %  ApproxNDCG %  WassRank     |
-    |           | STListNet                                                                 |
-    -----------------------------------------------------------------------------------------   
-
-
+    
     >>> Supported Datasets <<<
     -----------------------------------------------------------------------------------------
     | LETTOR    | MQ2007_Super %  MQ2008_Super %  MQ2007_Semi %  MQ2008_Semi                |
@@ -40,9 +37,9 @@ if __name__ == '__main__':
     -----------------------------------------------------------------------------------------
     | Yahoo_LTR | Set1 % Set2                                                               |
     -----------------------------------------------------------------------------------------
-    | ISTELLA_LTR | Istella_S % Istella % Istella_X                                         |
+    | ISTELLA_LTR | Istella_S | Istella | Istella_X                                         |
     -----------------------------------------------------------------------------------------
-
+    
     """
 
     cuda = None  # the gpu id, e.g., 0 or 1, otherwise, set it as None indicating to use cpu
@@ -51,32 +48,18 @@ if __name__ == '__main__':
 
     config_with_json = True  # specify configuration with json files or not
 
-    models_to_run = [
-        # 'RankMSE',
-        # 'RankNet',
-        'LambdaRank',
-        # 'ListNet',
-        # 'ListMLE',
-        # 'RankCosine',
-        # 'ApproxNDCG',
-        # 'WassRank',
-        # 'STListNet',
-        # 'LambdaLoss'
-    ]
-
     global_study = optuna.create_study(direction='maximize')
-    auto_evaluator = AutoLTREvaluator(cuda=cuda)
+    auto_evaluator = AutoTreeLTREvaluator(cuda=cuda)
 
     if config_with_json:  # specify configuration with json files
         # the directory of json files
         # dir_json = '/Users/dryuhaitao/WorkBench/Dropbox/CodeBench/GitPool/wildltr_ptranking/testing/ltr_adhoc/json/'
         # dir_json = '/Volumes/data_hdd/ptranking.github.io/testing/ltr_adhoc/json/'
         # dir_json = '/Users/solar/WorkBench/Dropbox/CodeBench/GitPool/wildltr_ptranking/testing/ltr_adhoc/json/'
-        dir_json = '/Volumes/data_hdd/ptranking/testing/ltr_auto/adhoc/json/'
+        dir_json = '/Volumes/data_hdd/ptranking/testing/ltr_auto/tree/json/'
 
-        for model_id in models_to_run:
-            auto_evaluator.run(global_study=global_study, auto_evaluator=auto_evaluator, debug=debug,
-                               model_id=model_id, config_with_json=True, dir_json=dir_json)
+        auto_evaluator.run(global_study=global_study, auto_evaluator=auto_evaluator, debug=debug,
+                               model_id='LightGBMLambdaMART', config_with_json=True, dir_json=dir_json)
     else:
         # data_id = 'MQ2007_Super'
         data_id = 'MQ2008_Super'
@@ -85,14 +68,13 @@ if __name__ == '__main__':
         # dir_data = '/Users/dryuhaitao/WorkBench/Corpus/' + 'LETOR4.0/MQ2007/'
         # dir_data = '/home/dl-box/WorkBench/Datasets/L2R/LETOR4.0/MQ2007/'
         #dir_data = '/Users/solar/WorkBench/Datasets/L2R/LETOR4.0/MQ2008/'
-        dir_data = '/Users/iimac/Workbench/Corpus/L2R/LETOR4.0/MQ2008/'
+        dir_data = '/Volumes/data_hdd/dataset/MQ2008/'
 
         ''' output directory '''
         # dir_output = '/Users/dryuhaitao/WorkBench/CodeBench/Bench_Output/NeuralLTR/Listwise/'
         # dir_output = '/home/dl-box/WorkBench/CodeBench/PyCharmProject/Project_output/Out_L2R/Listwise/'
         # dir_output = '/Users/solar/WorkBench/CodeBench/PyCharmProject/Project_output/Out_L2R/'
-        dir_output = '/Users/iimac/Workbench/CodeBench/Output/NeuralLTR/'
+        dir_output = '/Volumes/data_hdd/l2r_output/auto/'
 
-        for model_id in models_to_run:
-            auto_evaluator.run(global_study=global_study, auto_evaluator=auto_evaluator, debug=debug, model_id=model_id,
+        auto_evaluator.run(global_study=global_study, auto_evaluator=auto_evaluator, debug=debug, model_id='LightGBMLambdaMART',
                                config_with_json=False, data_id=data_id, dir_data=dir_data, dir_output=dir_output)
